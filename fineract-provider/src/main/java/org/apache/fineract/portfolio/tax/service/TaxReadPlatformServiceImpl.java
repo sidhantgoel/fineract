5fdbcc3e3fb019d11bd22180cca97636385646ad
Sidhant Goel <sidhant@hexcod.in>
2025-07-25 12:35:34 +0530

    Enhance JDBC query methods to use scrollable and updatable ResultSet for tax components and groups

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/tax/service/TaxReadPlatformServiceImpl.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/tax/service/TaxReadPlatformServiceImpl.java
@@ -52,7 +52,9 @@ public class TaxReadPlatformServiceImpl implements TaxReadPlatformService {
     @Override
     public List<TaxComponentData> retrieveAllTaxComponents() {
         String sql = "select " + TAX_COMPONENT_MAPPER.getSchema();
-        return this.jdbcTemplate.query(sql, TAX_COMPONENT_MAPPER); // NOSONAR
+        return this.jdbcTemplate.query(con -> con.prepareStatement(sql,
+                                                                   ResultSet.TYPE_SCROLL_SENSITIVE,
+                                                                   ResultSet.CONCUR_UPDATABLE), TAX_COMPONENT_MAPPER); // NOSONAR
     }
 
     @Override
@@ -70,7 +72,9 @@ public class TaxReadPlatformServiceImpl implements TaxReadPlatformService {
     @Override
     public List<TaxGroupData> retrieveAllTaxGroups() {
         String sql = "select " + TAX_GROUP_MAPPER.getSchema();
-        return this.jdbcTemplate.query(sql, TAX_GROUP_MAPPER); // NOSONAR
+        return this.jdbcTemplate.query(con -> con.prepareStatement(sql,
+                                                                   ResultSet.TYPE_SCROLL_SENSITIVE,
+                                                                   ResultSet.CONCUR_UPDATABLE), TAX_GROUP_MAPPER); // NOSONAR
     }
 
     @Override
